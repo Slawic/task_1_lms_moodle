a2490f1edf27c7db21c89933b0d1cffd74f8617b
Slavik <yaroslavmd@gmail.com>
2019-02-28 02:41:08 +0200

    add install & configure apache for MOODLE

--- Vagrantfile
+++ Vagrantfile
@@ -14,6 +14,7 @@ Vagrant.configure("2") do |config|
   # boxes at https://vagrantcloud.com/search.
   config.vm.box = "centos/7"
 
+
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
   # `vagrant box outdated`. This is not recommended.
@@ -23,21 +24,27 @@ Vagrant.configure("2") do |config|
   # within the machine from a port on the host machine. In the example below,
   # accessing "localhost:8080" will access port 80 on the guest machine.
   # NOTE: This will enable public access to the opened port
-  config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
+  config.vm.network "forwarded_port", guest: 80, host: 8081
+  # config.vm.network "forwarded_port", guest: 22, host: 2222
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine and only allow access
   # via 127.0.0.1 to disable public access
-  # config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
+  # config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "172.16.0.15"
 
+  
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
-  # config.vm.network "private_network", ip: "192.168.56.100"
+  # config.vm.network "private_network", ip: "172.16.0.150" 
 
   # Create a public network, which generally matched to bridged network.
   # Bridged networks make the machine appear as another physical device on
   # your network.
-  # config.vm.network "public_network"
+  # config.vm.network "public_network", ip: "xxx"
+  # default router
+ # config.vm.provision "shell",
+ #   run: "always",
+ #	inline: "route add default gw xxx"
 
   # Share an additional folder to the guest VM. The first argument is
   # the path on the host to the actual folder. The second argument is
@@ -49,14 +56,14 @@ Vagrant.configure("2") do |config|
   # backing providers for Vagrant. These expose provider-specific options.
   # Example for VirtualBox:
   #
-  config.vm.provider "virtualbox" do |vb|
-  #   # Display the VirtualBox GUI when booting the machine
-     vb.name = "mycentos7"
-  #   vb.gui = true
+   config.vm.provider "virtualbox" do |vb|
+  #  # Display the VirtualBox GUI when booting the machine
+  #  vb.gui = true
+     vb.name = "mytask01" 
   #
   #   # Customize the amount of memory on the VM:
      vb.memory = "2048"
-  end
+   end
   #
   # View the documentation for the provider you are using for more
   # information on available options.
@@ -64,7 +71,10 @@ Vagrant.configure("2") do |config|
   # Enable provisioning with a shell script. Additional provisioners such as
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
-  config.vm.provision "shell", path:"scenario.sh" 
-  # <<-SHELL
-  #SHELL
+  # config.vm.provision "shell", inline: <<-SHELL
+  #   apt-get update
+  #   apt-get install -y apache2
+  # SHELL
+  config.vm.provision "shell", path: "scenario.sh"
+
 end
--- scenario.sh
+++ scenario.sh
@@ -1,13 +1,106 @@
-  sudo yum clean all
-  sudo yum -y update
-  sudo rpm -Uvh  http://download.moodle.org/releases/latest/
-  sudo yum -y install httpd
-  sudo firewall-cmd --permanent --add-port=80/tcp
-  sudo firewall-cmd --permanent --add-port=443/tcp
-  sudo firewall-cmd --reload
-  sudo systemctl start httpd.service
-  sudo systemctl enable httpd
-  ##  sudo systemctl status httpd
-  ##  sudo systemctl stop httpd
-  ##  sudo yum -y install apache2
-
+#!/bin/bash
+# DB Params >
+DBNAME="moodle"
+DBUSER="UserDB"
+DBPASS="PassDB"
+# <
+# MOODLE Params >
+MOODLEUSER="Moodle"
+MOODLEPASS="Passw0rd"
+# <
+# install mc-commander >
+# sudo yum -y install mc
+# <
+# packets update >
+sudo yum clean all
+sudo yum -y update
+# <
+# install Apache >
+sudo yum -y install httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+# <
+# install PHP7.0 >
+sudo yum -y install epel-release
+sudo rpm -Uhv https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum -y install yum-utils
+sudo yum-config-manager --enable remi-php70
+sudo yum -y install php php-common php-intl php-zip php-soap php-xmlrpc php-opcache php-mbstring php-gd php-curl php-mysql php-xml
+# <
+# install MariaDB >
+sudo yum -y install mariadb-server
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+# creating MariaDB for MOODLE >
+sudo mysql -e "SET GLOBAL character_set_server = 'utf8mb4';"
+sudo mysql -e "SET GLOBAL innodb_file_format = 'BARRACUDA';"
+sudo mysql -e "SET GLOBAL innodb_large_prefix = 'ON';"
+sudo mysql -e "SET GLOBAL innodb_file_per_table = 'ON';"
+sudo mysql -e "CREATE DATABASE ${DBNAME};"
+sudo mysql -e "CREATE USER '${DBUSER}'@'localhost' IDENTIFIED BY '${DBPASS}';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON ${DBNAME}.* to '${DBUSER}'@'localhost';"
+sudo mysql -e "FLUSH PRIVILEGES;"
+# <
+################
+# Install App >
+curl https://download.moodle.org/download.php/direct/stable36/moodle-latest-36.tgz -o moodle-latest-36.tgz -s
+sudo tar -xvzf moodle-latest-36.tgz -C /var/www/html/
+#
+sudo /usr/bin/php /var/www/html/moodle/admin/cli/install.php \
+--lang=uk \
+--chmod=2770 \
+--wwwroot=http://localhost:8081/moodle \
+--dataroot=/var/moodledata \
+--dbtype=mariadb \
+--dbhost=localhost \
+--dbport=3306 \
+--dbname=${DBNAME} \
+--dbuser=${DBUSER} \
+--dbpass=${DBPASS} \
+--fullname=Moodle \
+--shortname=ymd \
+--summary=Moodle01 \
+--adminuser=${MOODLEUSER} \
+--adminpass=${MOODLEPASS} \
+--non-interactive \
+--agree-license
+#
+# <
+# >
+sudo chmod o+r /var/www/html/moodle/config.php
+###sudo chown -R apache:apache /var/www/html/
+sudo chcon -R -t httpd_sys_rw_content_t /var/moodledata
+sudo chown -R apache:apache /var/moodledata
+sudo chown -R apache:apache /var/www/
+# <
+# configuring Apache vhost>
+sudo systemctl stop httpd
+cd /etc/httpd/conf.d/
+sudo touch moodle.conf
+cat <<EOF | sudo tee -a /etc/httpd/conf.d/moodle.conf
+<VirtualHost *:8081>
+ServerAdmin admin@moodle.com
+DocumentRoot /var/www/html/moodle
+ServerName moodle.com
+ServerAlias www.moodle.com
+Alias /moodle "/var/www/html/moodle/"
+<Directory /var/www/html/moodle/>
+Options FollowSymLinks
+AllowOverride All
+</Directory>
+ErrorLog /var/log/httpd/moodle-error_log
+CustomLog /var/log/httpd/moodle-access_log common
+</VirtualHost>
+EOF
+# <
+# restart apache >
+sudo systemctl restart httpd
+# <
+# firewall >
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
+sudo firewall-cmd --zone=publicweb --add-service=ssh
+sudo firewall-cmd --permanent --zone=public --add-service=http 
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
+# <
